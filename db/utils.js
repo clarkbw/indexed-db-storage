c27dc15b3d19e912090a898f9a0f5497202bb25e
Bryan Clark <clarkbw@gmail.com>
2014-11-05 05:26:51 -0800

    adding header to utils file

--- db/utils.js
+++ db/utils.js
@@ -1,3 +1,12 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* jshint strict: true, esnext: true, newcap: false, globalstrict: true,
+   devel: true, node: true */
+
+"use strict";
+
 // https://mxr.mozilla.org/
 // mozilla-central/source/dom/indexedDB/IDBDatabase.cpp#606
 exports.READ_ONLY = "readonly";
